febdcd66cfb54456c9395b93f634d1310e65b683
groundkcm <karaminseo@naver.com>
2022-06-18 17:52:11 +0900

    터레인 추가 Obhect 수정

--- Object.cpp
+++ Object.cpp
@@ -61,10 +61,21 @@ void CMaterial::PrepareShaders(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandLi
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-CGameObject::CGameObject()
+CGameObject::CGameObject(int nMeshes)
 {
 	m_xmf4x4Transform = Matrix4x4::Identity();
 	m_xmf4x4World = Matrix4x4::Identity();
+
+	m_nMeshes = nMeshes;
+	m_ppMeshes = NULL;
+
+	if (m_nMeshes > 0)
+	{
+		m_ppMeshes = new CMesh * [m_nMeshes];
+
+		for (int i = 0; i < m_nMeshes; i++)
+			m_ppMeshes[i] = NULL;
+	}
 }
 
 CGameObject::~CGameObject()
@@ -79,6 +90,22 @@ CGameObject::~CGameObject()
 		}
 	}
 	if (m_ppMaterials) delete[] m_ppMaterials;
+
+	if (m_ppMeshes)
+	{
+		for (int i = 0; i < m_nMeshes; i++)
+		{
+			if (m_ppMeshes[i]) m_ppMeshes[i]->Release();
+			m_ppMeshes[i] = NULL;
+		}
+		delete[] m_ppMeshes;
+	}
+
+	if (m_pShader)
+	{
+		m_pShader->ReleaseShaderVariables();
+		m_pShader->Release();
+	}
 }
 
 void CGameObject::AddRef()
@@ -122,12 +149,32 @@ void CGameObject::SetMesh(CMesh *pMesh)
 	if (m_pMesh) m_pMesh->AddRef();
 }
 
+void CGameObject::SetMesh(int nIndex, CMesh* pMesh) {
+
+	if (m_ppMeshes)
+	{
+		if (m_ppMeshes[nIndex])
+			m_ppMeshes[nIndex]->Release();
+		m_ppMeshes[nIndex] = pMesh;
+
+		if (pMesh)
+			pMesh->AddRef();
+	}
+
+}
+
 void CGameObject::SetShader(CShader *pShader)
 {
 	m_nMaterials = 1;
 	m_ppMaterials = new CMaterial*[m_nMaterials];
 	m_ppMaterials[0] = new CMaterial();
 	m_ppMaterials[0]->SetShader(pShader);
+
+	if (m_pShader) m_pShader->Release();
+
+	m_pShader = pShader;
+
+	if (m_pShader) m_pShader->AddRef();
 }
 
 void CGameObject::SetShader(int nMaterial, CShader *pShader)
@@ -180,6 +227,19 @@ void CGameObject::Render(ID3D12GraphicsCommandList *pd3dCommandList, CCamera *pC
 	}
 	if (m_pSibling) m_pSibling->Render(pd3dCommandList, pCamera);
 	if (m_pChild) m_pChild->Render(pd3dCommandList, pCamera);
+
+	if (m_pShader)
+		m_pShader->Render(pd3dCommandList, pCamera);
+
+	//게임 객체가 포함하는 모든 메쉬를 렌더링한다.
+	if (m_ppMeshes)
+	{
+		for (int i = 0; i < m_nMeshes; i++)
+		{
+			if (m_ppMeshes[i])
+				m_ppMeshes[i]->Render(pd3dCommandList);
+		}
+	}
 }
 
 void CGameObject::CreateShaderVariables(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList)
@@ -188,6 +248,12 @@ void CGameObject::CreateShaderVariables(ID3D12Device *pd3dDevice, ID3D12Graphics
 
 void CGameObject::UpdateShaderVariables(ID3D12GraphicsCommandList *pd3dCommandList)
 {
+	XMFLOAT4X4 xmf4x4World;
+
+	XMStoreFloat4x4(&xmf4x4World, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4World)));
+
+	//객체의 월드 변환 행렬을 루트 상수(32-비트 값)를 통하여 셰이더 변수(상수 버퍼)로 복사한다.
+	pd3dCommandList->SetGraphicsRoot32BitConstants(0, 16, &xmf4x4World, 0);
 }
 
 void CGameObject::UpdateShaderVariable(ID3D12GraphicsCommandList *pd3dCommandList, XMFLOAT4X4 *pxmf4x4World)
@@ -211,6 +277,14 @@ void CGameObject::ReleaseUploadBuffers()
 
 	if (m_pSibling) m_pSibling->ReleaseUploadBuffers();
 	if (m_pChild) m_pChild->ReleaseUploadBuffers();
+	if (m_ppMeshes)
+	{
+		for (int i = 0; i < m_nMeshes; i++)
+		{
+			if (m_ppMeshes[i])
+				m_ppMeshes[i]->ReleaseUploadBuffers();
+		}
+	}
 }
 
 void CGameObject::UpdateTransform(XMFLOAT4X4 *pxmf4x4Parent)
@@ -310,6 +384,42 @@ void CGameObject::Rotate(XMFLOAT4 *pxmf4Quaternion)
 
 	UpdateTransform(NULL);
 }
+
+void CGameObject::GenerateRayForPicking(XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View, XMFLOAT3* pxmf3PickRayOrigin, XMFLOAT3* pxmf3PickRayDirection) 
+{
+
+	XMFLOAT4X4 xmf4x4WorldView = Matrix4x4::Multiply(m_xmf4x4World, xmf4x4View);
+	XMFLOAT4X4 xmf4x4Inverse = Matrix4x4::Inverse(xmf4x4WorldView);
+	XMFLOAT3 xmf3CameraOrigin(0.0f, 0.0f, 0.0f);
+
+	//카메라 좌표계의 원점을 모델 좌표계로 변환한다. 
+	*pxmf3PickRayOrigin = Vector3::TransformCoord(xmf3CameraOrigin, xmf4x4Inverse);
+
+	//카메라 좌표계의 점(마우스 좌표를 역변환하여 구한 점)을 모델 좌표계로 변환한다. 
+	*pxmf3PickRayDirection = Vector3::TransformCoord(xmf3PickPosition, xmf4x4Inverse);
+
+	//광선의 방향 벡터를 구한다. 
+	*pxmf3PickRayDirection = Vector3::Normalize(Vector3::Subtract(*pxmf3PickRayDirection, *pxmf3PickRayOrigin));
+
+}
+
+int CGameObject::PickObjectByRayIntersection(int index, XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View, float* pfHitDistance) 
+{
+
+	int nIntersected = 0;
+
+	if (m_ppMeshes[index])
+	{
+		XMFLOAT3 xmf3PickRayOrigin, xmf3PickRayDirection;
+		//모델 좌표계의 광선을 생성한다. 
+		GenerateRayForPicking(xmf3PickPosition, xmf4x4View, &xmf3PickRayOrigin, &xmf3PickRayDirection);
+		//모델 좌표계의 광선과 메쉬의 교차를 검사한다. 
+		nIntersected = m_ppMeshes[index]->CheckRayIntersection(xmf3PickRayOrigin, xmf3PickRayDirection, pfHitDistance);
+	}
+
+	return(nIntersected);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -804,3 +914,66 @@ void CMi24Object::OnInitialize()
 	m_pTailRotorFrame = FindFrame("Tail_Rotor");
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+CHeightMapTerrain::CHeightMapTerrain(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, LPCTSTR pFileName, int
+	nWidth, int nLength, int nBlockWidth, int nBlockLength, XMFLOAT3 xmf3Scale, XMFLOAT4 xmf4Color) : CGameObject(0) 
+{
+	//지형에 사용할 높이 맵의 가로, 세로의 크기이다.
+	m_nWidth = nWidth; m_nLength = nLength;
+
+	/*
+		지형 객체는 격자 메쉬들의 배열로 만들 것이다. nBlockWidth, nBlockLength는 격자 메쉬 하나의 가로, 세로 크 기이다.
+		cxQuadsPerBlock, czQuadsPerBlock은 격자 메쉬의 가로 방향과 세로 방향 사각형의 개수이다.
+	*/
+	int cxQuadsPerBlock = nBlockWidth - 1;
+	int czQuadsPerBlock = nBlockLength - 1;
+
+	//xmf3Scale는 지형을 실제로 몇 배 확대할 것인가를 나타낸다. 
+	m_xmf3Scale = xmf3Scale;
+
+	//지형에 사용할 높이 맵을 생성한다. 
+	m_pHeightMapImage = new CHeightMapImage(pFileName, nWidth, nLength, xmf3Scale);
+
+	//지형에서 가로 방향, 세로 방향으로 격자 메쉬가 몇 개가 있는 가를 나타낸다. 
+	long cxBlocks = (m_nWidth - 1) / cxQuadsPerBlock; long czBlocks = (m_nLength - 1) / czQuadsPerBlock;
+
+	//지형 전체를 표현하기 위한 격자 메쉬의 개수이다.
+	m_nMeshes = cxBlocks * czBlocks;
+
+	//지형 전체를 표현하기 위한 격자 메쉬에 대한 포인터 배열을 생성한다.
+	m_ppMeshes = new CMesh * [m_nMeshes];
+
+	for (int i = 0; i < m_nMeshes; i++)
+		m_ppMeshes[i] = NULL;
+
+	CHeightMapGridMesh* pHeightMapGridMesh = NULL;
+
+	for (int z = 0, zStart = 0; z < czBlocks; z++)
+	{
+		for (int x = 0, xStart = 0; x < cxBlocks; x++)
+		{
+			//지형의 일부분을 나타내는 격자 메쉬의 시작 위치(좌표)이다. 
+			xStart = x * (nBlockWidth - 1);
+			zStart = z * (nBlockLength - 1);
+
+			//지형의 일부분을 나타내는 격자 메쉬를 생성하여 지형 메쉬에 저장한다. 
+			pHeightMapGridMesh = new CHeightMapGridMesh(pd3dDevice, pd3dCommandList, xStart, zStart, nBlockWidth, nBlockLength, xmf3Scale, xmf4Color, m_pHeightMapImage);
+			SetMesh(x + (z * cxBlocks), pHeightMapGridMesh);
+		}
+	}
+	//지형을 렌더링하기 위한 셰이더를 생성한다. 
+	//CTerrainShader* pShader = new CTerrainShader();
+
+	//pShader->CreateShader(pd3dDevice, pd3dGraphicsRootSignature);
+	//SetShader(pShader);
+
+}
+
+CHeightMapTerrain::~CHeightMapTerrain(void) 
+{
+
+	if (m_pHeightMapImage)
+		delete m_pHeightMapImage;
+
+}
--- Object.h
+++ Object.h
@@ -6,6 +6,9 @@
 
 #include "Mesh.h"
 #include "Camera.h"
+class CMesh;
+class CCamera;
+class CPlayer;
 
 #define DIR_FORWARD					0x01
 #define DIR_BACKWARD				0x02
@@ -115,13 +118,17 @@ public:
 	void Release();
 
 public:
-	CGameObject();
+	CGameObject(int nMeshes = 1);
     virtual ~CGameObject();
 
 public:
 	char							m_pstrFrameName[64];
 
 	CMesh							*m_pMesh = NULL;
+
+	CShader							*m_pShader = NULL;
+	CMesh							**m_ppMeshes = NULL;
+	int								m_nMeshes = 0;
 
 	int								m_nMaterials = 0;
 	CMaterial						**m_ppMaterials = NULL;
@@ -138,6 +145,7 @@ public:
 	bool							m_pPlayerCollided = false;
 
 	void SetMesh(CMesh *pMesh);
+	virtual void SetMesh(int nIndex, CMesh* pMesh);
 	void SetShader(CShader *pShader);
 	void SetShader(int nMaterial, CShader *pShader);
 	void SetMaterial(int nMaterial, CMaterial *pMaterial);
@@ -177,6 +185,10 @@ public:
 	void Rotate(float fPitch = 10.0f, float fYaw = 10.0f, float fRoll = 10.0f);
 	void Rotate(XMFLOAT3 *pxmf3Axis, float fAngle);
 	void Rotate(XMFLOAT4 *pxmf4Quaternion);
+
+	void GenerateRayForPicking(XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View, XMFLOAT3* pxmf3PickRayOrigin, XMFLOAT3* pxmf3PickRayDirection);
+	int PickObjectByRayIntersection(int index, XMFLOAT3& xmf3PickPosition, XMFLOAT4X4& xmf4x4View, float* pfHitDistance);
+
 
 	CGameObject *GetParent() { return(m_pParent); }
 	void UpdateTransform(XMFLOAT4X4 *pxmf4x4Parent=NULL);
@@ -294,4 +306,38 @@ public:
 
 public:
 	virtual void OnInitialize();
+};
+
+class CHeightMapTerrain : public CGameObject {
+
+public:
+	CHeightMapTerrain(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature, LPCTSTR pFileName, int nWidth, int nLength, int nBlockWidth, int nBlockLength, XMFLOAT3 xmf3Scale, XMFLOAT4 xmf4Color); virtual ~CHeightMapTerrain();
+
+private:
+	//지형의 높이 맵으로 사용할 이미지이다.
+	CHeightMapImage* m_pHeightMapImage;
+
+	//높이 맵의 가로와 세로 크기이다. 
+	int m_nWidth;
+	int m_nLength;
+
+	//지형을 실제로 몇 배 확대할 것인가를 나타내는 스케일 벡터이다. 
+	XMFLOAT3 m_xmf3Scale;
+
+public:
+	//지형의 높이를 계산하는 함수이다(월드 좌표계). 높이 맵의 높이에 스케일의 y를 곱한 값이다. 
+	float GetHeight(float x, float z) { return(m_pHeightMapImage->GetHeight(x / m_xmf3Scale.x, z / m_xmf3Scale.z) * m_xmf3Scale.y); }
+
+	//지형의 법선 벡터를 계산하는 함수이다(월드 좌표계). 높이 맵의 법선 벡터를 사용한다.
+	XMFLOAT3 GetNormal(float x, float z) { return(m_pHeightMapImage->GetHeightMapNormal(int(x / m_xmf3Scale.x), int(z / m_xmf3Scale.z))); }
+
+	int GetHeightMapWidth() { return(m_pHeightMapImage->GetHeightMapWidth()); }
+	int GetHeightMapLength() { return(m_pHeightMapImage->GetHeightMapLength()); }
+
+	XMFLOAT3 GetScale() { return(m_xmf3Scale); }
+
+	//지형의 크기(가로/세로)를 반환한다. 높이 맵의 크기에 스케일을 곱한 값이다. 
+	float GetWidth() { return(m_nWidth * m_xmf3Scale.x); }
+	float GetLength() { return(m_nLength * m_xmf3Scale.z); }
+
 };
