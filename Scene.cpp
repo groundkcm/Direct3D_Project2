219d33e2c8f53cac40d711eff3d2b476f7d13718
groundkcm <karaminseo@naver.com>
2022-06-18 22:32:27 +0900

    터레인 렌더링 준비

--- Scene.cpp
+++ Scene.cpp
@@ -73,7 +73,6 @@ void CScene::BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *p
 	m_pd3dGraphicsRootSignature = CreateGraphicsRootSignature(pd3dDevice);
 	XMFLOAT3 xmf3Scale(8.0f, 2.0f, 8.0f); XMFLOAT4 xmf4Color(0.0f, 0.2f, 0.0f, 0.0f);
 
-	//???
 	CMaterial::PrepareShaders(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature);
 
 
@@ -83,10 +82,10 @@ void CScene::BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *p
 	지형 전체는 가로 방향으로 16개, 세로 방향으로 16의 격자 메 쉬를 가진다.
 	지형을 구성하는 격자 메쉬의 개수는 총 256(16x16)개가 된다.
 	*/
-	m_pTerrain = new CHeightMapTerrain(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature, _T("/Model/Terrain/HeightMap.raw"), 257, 257, 17, 17, xmf3Scale, xmf4Color);
+	m_pTerrain = new CHeightMapTerrain(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature, _T("./Model/Terrain/HeightMap.raw"), 257, 257, 17, 17, xmf3Scale, xmf4Color);
 #else
 	//지형을 하나의 격자 메쉬(257x257)로 생성한다. 
-	m_pTerrain = new CHeightMapTerrain(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature, _T("/Model/Terrain/HeightMap.raw"), 257, 257, 257, 257, xmf3Scale, xmf4Color);
+	m_pTerrain = new CHeightMapTerrain(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature, _T("./Model/Terrain/HeightMap.raw"), 257, 257, 257, 257, xmf3Scale, xmf4Color);
 #endif
 	m_nShaders = 1;
 	m_pShaders = new CObjectsShader[m_nShaders];
@@ -110,7 +109,6 @@ void CScene::BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *p
 	pApacheObject->Rotate(0.0f, 180.0f, 0.0f);
 	m_ppGameObjects[0] = pApacheObject;
 
-	//???
 	CreateShaderVariables(pd3dDevice, pd3dCommandList);
 }
 
@@ -123,8 +121,6 @@ void CScene::ReleaseObjects()
 		for (int i = 0; i < m_nGameObjects; i++) if (m_ppGameObjects[i]) m_ppGameObjects[i]->Release();
 		delete[] m_ppGameObjects;
 	}
-
-	//ReleaseShaderVariables();
 
 	if (m_pLights) delete[] m_pLights;
 	if (m_pTerrain)
